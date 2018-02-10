e2380e748f07e013184e61cd4db683afa560c039
Máté Németh <nmate91@gmail.com>
2018-02-10 14:49:29 +0100

    Extended with list logic

---
+++ MergeSort/MergeSorter_Array.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    class MergeSorter_Array
+    {
+        public List<int> Merge(List<int> leftNumbers, List<int> rightNumbers)
+        {
+            if (leftNumbers.Count == 0) { return leftNumbers; }
+            if (rightNumbers.Count == 0) { return rightNumbers; }
+            List<int> result = new List<int>();
+            int i = 0;
+            int j = 0;
+            while (result.Count < (leftNumbers.Count + rightNumbers.Count))
+            {
+                int right = rightNumbers.Count > j ? rightNumbers[j] : int.MinValue;
+                int left = leftNumbers.Count > i ? leftNumbers[i] : int.MinValue;
+                if (left > right)
+                {
+                    result.Add(left);
+                    ++i;
+                }
+                else
+                {
+                    result.Add(right);
+                    ++j;
+                }
+                if (i == leftNumbers.Count) { result.Union(leftNumbers); }
+                if (j == rightNumbers.Count) { result.Union(rightNumbers); }
+            }
+            return result;
+        }
+
+        public List<int> Sort(List<int> numbers)
+        {
+            if (numbers.Count < 2) { return numbers; }
+            int middle = (int)(numbers.Count / 2);
+            List<int> rightNumbers = Sort(numbers.Skip(middle).ToList());
+            List<int> leftNumbers = Sort(numbers.Except(rightNumbers).ToList());
+            return Merge(leftNumbers, rightNumbers);
+        }
+    }
+}
