edcf9312ec0cbcfdde5ee1608960bb84a029203e
Máté Németh <nmate91@gmail.com>
2018-02-10 01:27:30 +0100

    Add project files.

---
+++ MergeSort/MergeSorter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    public class MergeSorter
+    {
+        public LinkedList<int> Merge(LinkedList<int> leftNumbers, LinkedList<int> rightNumbers)
+        {
+            if(leftNumbers.Count == 0) { return leftNumbers; }
+            if(rightNumbers.Count == 0) { return rightNumbers; }
+            LinkedList<int> result = new LinkedList<int> { };
+
+            int i = 0;
+            int j = 0;
+
+            while (result.Count < (leftNumbers.Count + rightNumbers.Count))
+            {
+                int right = rightNumbers.Count > j ? rightNumbers.ElementAt(j) : int.MinValue;
+                int left = leftNumbers.Count > i ? leftNumbers.ElementAt(i) : int.MinValue;
+                if (left > right)
+                {
+                    result.AddLast(left);
+                    ++i;
+                }
+                else
+                {
+                    result.AddLast(right);
+                    ++j;
+                }
+                if(i == leftNumbers.Count) { result.Union(leftNumbers); }
+                if(j == rightNumbers.Count) { result.Union(rightNumbers); }
+            }
+            return result;
+        }
+
+        public LinkedList<int> Sort(LinkedList<int> numbers)
+        {
+            if(numbers.Count < 2) { return numbers; }
+
+            int middle = (int)(numbers.Count / 2);
+            LinkedList<int> rightNumbers = Sort(new LinkedList<int>(numbers.Skip(middle)));
+            LinkedList<int> leftNumbers = Sort(new LinkedList<int>(numbers.Except(rightNumbers)));
+
+            return Merge(leftNumbers, rightNumbers);
+        }
+    }
+}
---
+++ MergeSort/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MergeSorter sorter = new MergeSorter();
+            int[] array = new int[] { 3, 4, 5, -1, 2, 8, 3, 7, 6 };
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+            var sorted = sorter.Sort(new LinkedList<int>(array));
+            foreach (var item in sorted)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
